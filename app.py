da252c55cb05e753dd76abf00baf3f51498570c9
tasneem643 <ahmedtasneem643@gmail.com>
2026-06-12 21:43:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from docx import Document
 from pptx import Presentation
 from PIL import Image
 
-appdb = Flask(__name__)
+app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
